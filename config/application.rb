18357c2ea28a3dfd9799f1e2b519aa6f5294d40d
DangLienMinh <uit11520232@gmail.com>
2015-06-11 08:22:13 +0700

    ok

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Trungcapnghe
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end

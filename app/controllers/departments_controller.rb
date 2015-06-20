c4dbcc8c18a1e80a90af98eb0bae50238b25499a
DangLienMinh <uit11520232@gmail.com>
2015-06-20 15:23:00 +0700

    change code tidy

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,33 +1,14 @@
 class DepartmentsController < ApplicationController
-  # def phongdaotao
-  #   general_tt_tb(1)
-  #   @mota=Department.where(id: )
-  # end
-
   def show
     @department=Department.find(params[:id])
-    @thongbao=Post.where("category_id = ? and department_id=?",2,@department.id).order(created_at: :desc).limit(5)
-    @tintuc=Post.where("category_id = ? and department_id=?",3,@department.id).order(created_at: :desc).limit(5)
+    @category=Category.joins(:posts).where("posts.department_id=?",@department.id).uniq
+    
+    #@category = Category.includes(:posts).select{|c| c.posts.where(['department_id = ?',@department.id])}
+    #@category=Category.all
+    #@post=Post.where("department_id=?",@department.id).order(created_at: :desc).limit(5)
+    # @thongbao=Post.where("category_id = ? and department_id=?",2,@department.id).order(created_at: :desc).limit(5)
+    # @tintuc=Post.where("category_id = ? and department_id=?",3,@department.id).order(created_at: :desc).limit(5)
+    # @tuyendung=Post.where("category_id = ? and department_id=?",4,@department.id).order(created_at: :desc).limit(5)
+    # @tuyensinh=Post.where("category_id = ? and department_id=?",1,@department.id).order(created_at: :desc).limit(5)
   end
-
-  # def phongtaivu
-  #   general_tt_tb(2)
-  # end
-
-  # def tochuchanhchinh
-  #   general_tt_tb(3)
-  # end
-
-  # def quanlihs
-  #   general_tt_tb(4)
-  # end
-
-  # def qttb
-  #   general_tt_tb(5)
-  # end
-
-  # def general_tt_tb(department_id)
-  #   @thongbao=Post.where("category_id = ? and department_id=?",2,department_id).order(created_at: :desc).limit(5)
-  #   @tintuc=Post.where("category_id = ? and department_id=?",3,department_id).order(created_at: :desc).limit(5)
-  # end
 end

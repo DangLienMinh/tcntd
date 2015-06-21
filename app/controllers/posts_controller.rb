8dc253d05c45f63ccd9775015d9bcbf2a4c3cf76
thuykaka <thuykaka.uit@gmail.com>
2015-06-21 16:47:06 +0700

    changeeee

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,19 +10,19 @@ class PostsController < ApplicationController
     @allpost=@category.posts.where("id != ? and department_id=?",@post.id,@post.department_id).order(:created_at).limit(5)
   end
   def showalltintuc
-    @posttd=Post.where("category_id = ?",3).paginate(:page => params[:page], :per_page => 2)
+    @posttd=Post.where("category_id = ?",3).order(:created_at).paginate(:page => params[:page], :per_page => 4)
 
   end
   def showallthongbao
-    @posttd=Post.where("category_id = ?",2).paginate(:page => params[:page], :per_page => 2)
+    @posttd=Post.where("category_id = ?",2).order(:created_at).paginate(:page => params[:page], :per_page => 4)
 
   end
   def showalltuyensinh
-    @posttd=Post.where("category_id = ?",1).paginate(:page => params[:page], :per_page => 2)
+    @posttd=Post.where("category_id = ?",1).order(:created_at).paginate(:page => params[:page], :per_page => 4)
 
   end
   def showalltuyendung
-    @posttd=Post.where("category_id = ?",4).paginate(:page => params[:page], :per_page => 2)
+    @posttd=Post.where("category_id = ?",4).order(:created_at).paginate(:page => params[:page], :per_page => 4)
 
   end
 end

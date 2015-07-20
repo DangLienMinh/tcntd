16eb0eed63e9f93a7c14b7e73970207c5d9bc3e8
DangLienMinh <uit11520232@gmail.com>
2015-07-20 09:50:34 +0700

    change 18/8

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 201510611111111063138) do
+ActiveRecord::Schema.define(version: 20151061110631603) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"

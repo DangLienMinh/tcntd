d74486ec34627a9439f5364a8f976ea4f20cec86
root <root@CENTOS6-64.localdomain>
2015-08-06 14:53:56 +0700

    stash schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 201510611111111063138) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
     t.text     "body"

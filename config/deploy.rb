f81fd9cbe9f48b3c75318a13317b402246a90260
ThuyKaKa <thuykaka.uit@gmail.com>
2015-08-07 15:23:14 +0700

    change capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.2.1'
 
 set :application, 'tcntd'
 set :repo_url, 'git@heroku.com:tcntd.git'
-set :rvm_ruby_version, 'ruby-2.1.1@jinji-gemset'
+set :rvm_ruby_version, 'ruby-2.1.0@jinji-gemset'
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
